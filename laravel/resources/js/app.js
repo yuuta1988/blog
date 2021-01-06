c14f7ce2d385cf291796971858b1db08b9bfd36d
Yuuta Sawamura <>
2021-01-06 20:57:53 +0900

    [add]Vue.jsとBootstapの導入

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('header-component', require('./components/HeaderComponent.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
