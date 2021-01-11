302a054943d4a05d2a4e93023011275d94890c24
Yuuta Sawamura <>
2021-01-11 17:29:30 +0900

    [add]Bootstapの導入

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,12 +1,9 @@
-require("./bootstrap");
+require('./bootstrap');
 
-window.Vue = require("vue");
+window.Vue = require('vue');
 
-Vue.component(
-    "header-component",
-    require("./components/HeaderComponent.vue").default
-);
+Vue.component('header-component', require('./components/HeaderComponent.vue').default);
 
 const app = new Vue({
-    el: "#app"
+    el: '#app',
 });
